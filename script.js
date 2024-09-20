93868f136b6121bc6aae78d699a8de40811044ad
waydehueydeee <sashakinzalov_2002@mail.ru>
2024-09-20 15:41:55 +0300

    first push

---
+++ script.js
@@ -0,0 +1,82 @@
+let formButton = document.querySelector(".form__button");
+let values = [];
+keys = [
+  "contract_number",
+  "contract_date",
+  "parent_name",
+  "parent_phone",
+  "parent_email",
+  "registration_address",
+  "passport_series",
+  "passport_number",
+  "passport_issue_date",
+  "passport_place_of_issue",
+  "children_name",
+  "subject_name",
+  "rate_name",
+  "course_name",
+  "course_start_date",
+  "course_duration",
+  "course_price",
+  "parent_initials",
+];
+
+formButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const inputs = document.querySelectorAll(".form__conteiner-input");
+
+  for (let i = 0; i < inputs.length; i++) {
+    values.push(inputs[i].value);
+  }
+
+  const initials = inputs[2].value.split(" ");
+  const parent_initials =
+    initials[1][0] + "." + initials[2][0] + "." + initials[0];
+  values.push(parent_initials);
+
+  let change_date = values[1].split("-");
+  change_date = change_date[2] + "." + change_date[1] + "." + change_date[0];
+  values[1] = change_date;
+
+  let change_date2 = values[8].split("-");
+  change_date2 =
+    change_date2[2] + "." + change_date2[1] + "." + change_date2[0];
+  values[8] = change_date2;
+
+  let change_date3 = values[14].split("-");
+  change_date3 =
+    change_date3[2] + "." + change_date3[1] + "." + change_date3[0];
+  values[14] = change_date3;
+
+  const dict_values = keys.reduce((acc, key, index) => {
+    acc[key] = values[index]; // Присваиваем значение из массива values по индексу
+    return acc; // Возвращаем аккумулятор для следующей итерации
+  }, {});
+
+  const jsonData = JSON.stringify(dict_values);
+  fetch("/api/send-data", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: jsonData,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Ошибка при загрузке файла");
+      }
+      return response.blob(); // Получаем blob из ответа
+    })
+    .then((blob) => {
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "modified_contract.docx"; // Имя файла при скачивании
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    })
+    .catch((error) => {
+      console.error("Ошибка:", error);
+    });
+});
---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const PizZip = require("pizzip");
+const Docxtemplater = require("docxtemplater");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/api/send-data", (req, res) => {
+  const data = req.body;
+  console.log(data);
+
+  const templatePath = path.resolve(__dirname, "Contract.docx");
+  const content = fs.readFileSync(templatePath, "binary");
+
+  const zip = new PizZip(content);
+  const doc = new Docxtemplater(zip);
+
+  doc.setData({
+    contract_number: data.contract_number,
+    contract_date: data.contract_date,
+    parent_name: data.parent_name,
+    parent_phone: data.parent_phone,
+    parent_email: data.parent_email,
+    registration_address: data.registration_address,
+    passport_series: data.passport_series,
+    passport_number: data.passport_number,
+    passport_issue_date: data.passport_issue_date,
+    passport_place_of_issue: data.passport_place_of_issue,
+    children_name: data.children_name,
+    subject_name: data.subject_name,
+    rate_name: data.rate_name,
+    course_name: data.course_name,
+    course_start_date: data.course_start_date,
+    course_duration: data.course_duration,
+    course_price: data.course_price,
+    parent_initials: data.parent_initials,
+  });
+
+  try {
+    doc.render();
+  } catch (error) {
+    console.error("Error rendering document:", error);
+    return res.status(500).send("Ошибка при заполнении документа.");
+  }
+
+  const buf = doc.getZip().generate({ type: "nodebuffer" });
+
+  const outputPath = path.resolve(__dirname, "output.docx");
+  fs.writeFileSync(outputPath, buf);
+
+  res.setHeader(
+    "Content-Disposition",
+    'attachment; filename="modified_contract.docx"'
+  );
+  res.download(outputPath, "modified_contract.docx");
+});
+
+app.listen(8000, () => {
+  console.log("Server started on: http://localhost:8000");
+});
